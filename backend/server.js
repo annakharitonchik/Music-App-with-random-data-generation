93af86fa4f6de0e4a2117b04ca388aed0965c390
Anna <zharitonchik@icloud.com>
2026-06-20 17:34:50 +0300

    feat: run server

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+import dotenv from "dotenv";
+import cors from "cors";
+import express from "express";
+import songsRouter from "./src/routes/songs.routes.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+
+app.use("/songs", songsRouter);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(
+    `Server listening on port ${PORT} http://localhost:5000/songs?page=1&quantity=10&seed=123&rlikes=5.7`,
+  );
+});
