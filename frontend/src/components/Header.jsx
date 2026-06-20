b0241ab890b0a75e5a4f65583d5fe926b03848f0
Anna <zharitonchik@icloud.com>
2026-06-20 17:41:22 +0300

    feat: create Header

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <tr>
+      <th>#</th>
+      <th>Song</th>
+      <th>Artist</th>
+      <th>Album</th>
+      <th>Genre</th>
+    </tr>
+  );
+};
+export default Header;
